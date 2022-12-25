d7a1a17a69998fc032e3d96322611363ef0ce56b
alex-killer <onefrank@bk.ru>
2022-12-25 17:23:36 +0700

    Initial commit

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,20 @@
+@extends('layout')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <h1>Edit Image</h1>
+                <img src="/{{ $imageInView->image }}" alt="" class="img-thumbnail">
+                <form action="/update/{{ $imageInView->id }}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-control">
+                        <input type="file" name="image">
+                    </div>
+
+                    <button type="submit" class="btn btn-success">Edit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
